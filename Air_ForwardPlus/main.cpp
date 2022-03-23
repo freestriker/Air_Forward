9f263150ed29c6bee5465c07951e45efb185ffb7
FREEstriker <2317416569@qq.com>
2022-03-23 17:16:50 +0800

    添加RTTR库

---
+++ Air_ForwardPlus/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <rttr/registration>
+#include <iostream>
+static void f() { std::cout << "Hello World" << std::endl; }
+using namespace rttr;
+RTTR_REGISTRATION
+{
+    using namespace rttr;
+    registration::method("f", &f);
+}
+int main()
+{
+    type::invoke("f", {});
+}
+// outputs: "Hello World"
