fc03cab3cc0615daabc69b7a95ff35293667f50b
FREEstriker <2317416569@qq.com>
2022-05-24 01:06:24 +0800

    修改cout

--- Air_Forward/source/utils/Log.cpp
+++ Air_Forward/source/utils/Log.cpp
@@ -9,7 +9,7 @@ void Utils::Log::Message(std::string info, VkResult logCondition)
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
 		std::string s = "Message: " + info + " Error code: " + std::to_string(static_cast<int>(logCondition)) + ".";
-		std::cerr << s << std::endl;
+		std::cout << s << std::endl;
 	}
 }
 
@@ -18,14 +18,14 @@ void Utils::Log::Message(std::string info, bool logCondition)
 	if (logCondition)
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
-		std::cerr << "Message: " + info << std::endl;
+		std::cout << "Message: " + info << std::endl;
 	}
 }
 
 void Utils::Log::Message(std::string info)
 {
 	std::unique_lock<std::mutex> lock(_mutex);
-	std::cerr << "Message: " + info << std::endl;
+	std::cout << "Message: " + info << std::endl;
 }
 
 void Utils::Log::Exception(std::string info, VkResult logCondition)
@@ -34,7 +34,7 @@ void Utils::Log::Exception(std::string info, VkResult logCondition)
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
 		std::string s = "Exception: " + info + " Error code: " + std::to_string(static_cast<int>(logCondition)) + ".";
-		std::cerr << s << std::endl;
+		std::cout << s << std::endl;
 	}
 }
 
@@ -43,12 +43,12 @@ void Utils::Log::Exception(std::string info, bool logCondition)
 	if (logCondition)
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
-		std::cerr << "Exception: " + info << std::endl;
+		std::cout << "Exception: " + info << std::endl;
 	}
 }
 
 void Utils::Log::Exception(std::string info)
 {
 	std::unique_lock<std::mutex> lock(_mutex);
-	std::cerr << "Exception: " + info << std::endl;
+	std::cout << "Exception: " + info << std::endl;
 }
